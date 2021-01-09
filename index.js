a07ea430a0a8691e41983eb7f7ad1bdbb2aac7ec
Gianluca <giallotech@gmail.com>
2021-01-09 17:48:11 +0100

    updated full stack version

--- index.js
+++ index.js
@@ -18,12 +18,6 @@ app.use(
 	)
 );
 
-const generateId = (min, max) => {
-	min = Math.ceil(min);
-	max = Math.floor(max);
-	return Math.floor(Math.random() * (max - min) + min);
-};
-
 app.post("/api/persons", (request, response, next) => {
 	const body = request.body;
 
@@ -59,7 +53,7 @@ app.get("/api/persons/:id", (request, response, next) => {
 
 app.delete("/api/persons/:id", (request, response, next) => {
 	Person.findByIdAndRemove(request.params.id)
-		.then((result) => {
+		.then(() => {
 			response.status(204).end();
 		})
 		.catch((error) => next(error));
@@ -108,7 +102,7 @@ app.use(unknownEndpoint);
 const errorHandler = (error, request, response, next) => {
 	console.error(error.message);
 
-	if (error.name === "CastError" && error.kind == "ObjectId") {
+	if (error.name === "CastError" && error.kind === "ObjectId") {
 		return response.status(400).send({ error: "malformatted id" });
 	} else if (error.name === "ValidationError") {
 		return response.status(400).json({ error: error.message });
--- models/person.js
+++ models/person.js
@@ -1,36 +1,36 @@
-const mongoose = require("mongoose");
-const uniqueValidator = require("mongoose-unique-validator");
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
 
-const url = process.env.MONGODB_URI;
+const url = process.env.MONGODB_URI
 
 mongoose
-	.connect(url, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useFindAndModify: false,
-		useCreateIndex: true,
-	})
-	.then((result) => {
-		console.log("connected to MongoDB");
-	})
-	.catch((error) => {
-		console.log("error connecting to MongoDB:", error.message);
-	});
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = mongoose.Schema({
-	name: { type: String, minlength: 3, required: true, unique: true },
-	number: { type: String, required: true, minlength: 8 },
-});
+  name: { type: String, minlength: 3, required: true, unique: true },
+  number: { type: String, required: true, minlength: 8 },
+})
 
-personSchema.set("toJSON", {
-	transform: (document, returnedObject) => {
-		returnedObject.id = returnedObject._id.toString();
-		delete returnedObject._id;
-		delete returnedObject.__v;
-	},
-});
+personSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
 
 // Apply the uniqueValidator plugin to personSchema.
-personSchema.plugin(uniqueValidator);
+personSchema.plugin(uniqueValidator)
 
-module.exports = mongoose.model("Person", personSchema);
+module.exports = mongoose.model('Person', personSchema)
